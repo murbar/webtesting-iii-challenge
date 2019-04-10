d22d032f7a77cab5d26dbee9985d2b90d0be3b70
Joel Bartlett <joel@murbar.com>
2019-04-10 18:20:10 -0400

    Improve display tests

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,35 +1,63 @@
 import React from 'react';
 import Display from './Display';
 import { render } from 'react-testing-library';
+import 'jest-dom/extend-expect';
 
 describe('Display', () => {
-  it('displays unlocked by default', () => {
-    const { getByText } = render(<Display />);
-    getByText('Unlocked');
+  describe('Locked/Unlocked indicator', () => {
+    it('displays unlocked by default', () => {
+      const { getByText } = render(<Display />);
+      getByText('Unlocked');
+    });
+
+    it('displays locked when locked', () => {
+      const { getByText } = render(<Display locked={true} />);
+      getByText('Locked');
+    });
+
+    it('is styled red when locked', () => {
+      const { getByText } = render(<Display locked={true} />);
+      const display = getByText('Locked');
+      expect(display).toHaveClass('red-led');
+    });
+
+    it('displays unlocked when unlocked', () => {
+      const { getByText } = render(<Display locked={false} />);
+      getByText('Unlocked');
+    });
+
+    it('is styled green when unlocked', () => {
+      const { getByText } = render(<Display locked={false} />);
+      const display = getByText('Unlocked');
+      expect(display).toHaveClass('green-led');
+    });
   });
 
-  it('displays open by default', () => {
-    const { getByText } = render(<Display />);
-    getByText('Open');
-  });
+  describe('Open/Closed indicator', () => {
+    it('displays open by default', () => {
+      const { getByText } = render(<Display />);
+      getByText('Open');
+    });
+    it('displays open when open', () => {
+      const { getByText } = render(<Display closed={false} />);
+      getByText('Open');
+    });
 
-  it('displays locked when locked', () => {
-    const { getByText } = render(<Display locked={true} />);
-    getByText('Locked');
-  });
+    it('is styled green when open', () => {
+      const { getByText } = render(<Display closed={false} />);
+      const display = getByText('Open');
+      expect(display).toHaveClass('green-led');
+    });
 
-  it('displays unlocked when unlocked', () => {
-    const { getByText } = render(<Display locked={false} />);
-    getByText('Unlocked');
-  });
+    it('displays closed when closed', () => {
+      const { getByText } = render(<Display closed={true} />);
+      getByText('Closed');
+    });
 
-  it('displays open when open', () => {
-    const { getByText } = render(<Display closed={false} />);
-    getByText('Open');
-  });
-
-  it('displays closed when closed', () => {
-    const { getByText } = render(<Display closed={true} />);
-    getByText('Closed');
+    it('is styled red when closed', () => {
+      const { getByText } = render(<Display closed={true} />);
+      const display = getByText('Closed');
+      expect(display).toHaveClass('red-led');
+    });
   });
 });
